9b0a661d82d040cdda5c7499f9553c0d52819892
Mateusz Zajac <deejay.zajac@gmail.com>
2012-02-05 21:56:20 +0100

    Added first program

---
+++ lab7-1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#define ILE 15
+
+main () {
+  double tab[ILE], x;  int k, n, m;
+  printf("\nSORTOWANIE BABELKOWE");
+  printf("\nPodaj %i liczb rzeczywistych: ", ILE);
+  for (k=0; k<ILE; k=k+1)
+    scanf("%lf", &tab[k]);
+
+  n=ILE;
+  while (n>1) {
+    m=1;
+    for (k=1; k<n; k=k+1)
+      if (tab[k-1] > tab[k]) {
+        x=tab[k-1]; tab[k-1]=tab[k]; tab[k]=x;
+        m=k;
+      }
+    n=m;
+  }
+  
+  printf("\nWynik sortowania:\n");
+  for (k=0; k<ILE; k=k+1)
+    printf("  %lf\n", tab[k]);
+  printf("\n");
+}
+
+/* k - reprezentujê liczbê zmian - sortowañ w pêtli. Je¿eli nie bêdzie zmian to k=1 a warunek pêtli bêdzie przerwany*/
