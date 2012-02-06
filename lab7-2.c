f6d6536a6f4bfe57d6e37189141ebae9b147c70c
Mateusz Zajac <deejay.zajac@gmail.com>
2012-02-06 18:39:44 +0100

    zle zalozenie sortowanie - poprawione

--- lab7-2.c
+++ lab7-2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 main(){
 
-	int a=0,b,c=0,d,e,temp=0,temp1=0,temp2,tabela[1000],przenoszenie[1000],ILE;
+	int a=0,b,c=0,d,e,temp=0,temp1=0,temp2,tabela[1000]={0},przenoszenie[1000],ILE;
 
 	printf("Podaj ilosc posortowanych liczb do ktorych rozpoczniesz wstawianie\n");
 	scanf("%i",&a);
@@ -17,20 +17,33 @@ main(){
 		scanf("%i",&temptab[temp]);
 	}
 	temp=0;
+	printf("temp=%i   temp1=%i\n",temp,temp1);
 	while(b>c){
-		if(temptab[temp]<tabela[temp1]){
+		printf("Kraze w petli\n");
+		if(temptab[temp]>=tabela[temp1]){
 			temp1++;
+			printf("temp1++\n");
 		}
-		else if(temptab[temp]>=tabela[temp1]){
+		else if(temptab[temp]<tabela[temp1]){
+			printf("1\n");
 			c++;								/*Zwiekszamy ilosc wstawionych liczb*/
-			temp1=e;							/*temp1=e - bysmy nie utracili wartosci orginalu*/
-			for(d=0;e>d;d++){					/*Przepisujemy nie przejrzane liczby z tabeli*/
-				przenoszenie[d]=tabela[e];		/*do ktorej wstawiamy do tymczasowe tabeli*/
+			e=temp1;							/*temp1=e - bysmy nie utracili wartosci orginalu*/
+			for(d=0;a+b-temp1>=d;d++){					/*Przepisujemy nie przejrzane liczby z tabeli*/
+				przenoszenie[d]=tabela[e];		/*ktore wstawiamy do tymczasowej tabeli*/
+				e++;
 			}
+			e=temp1;
+			printf("temp=%i   temp1=%i\n",temp,temp1);
 			tabela[temp1]=temptab[temp];		/*Wstawiamy wartosc*/
-			for(d=0;e>d;d++){					/*Dopisujemy reszte tabeli*/
+			for(d=0;a+b-temp1>=d;d++){					/*Dopisujemy reszte tabeli*/
 				tabela[e+1]=przenoszenie[d];	/*i otrzmyjemy tabele o 1 element wieksza*/
+				e++;
 			}
 			temp++;
 		}
+	}
+	for(temp=0;a+b>temp;temp++){
+		printf("%i  ",tabela[temp]);
+	}
+	printf("\n");
 }
