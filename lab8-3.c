f87044ce0e236b3920d2f05d5f181b467eeacc1d
Mateusz Zajac <deejay.zajac@gmail.com>
2012-02-08 10:17:48 +0100

    dodane zabezpieczenie przed powtarzaniem par liter

--- lab8-3.c
+++ lab8-3.c
@@ -2,7 +2,7 @@
 //INF UG 2012
 #include<stdio.h>
 int badania(int a, int temp, char tabela[]){
-	int wynik=0,temp1=temp;
+	int wynik=0,temp1=temp,temp2;
 	while(temp1<=a){
 		if(tabela[temp1]==tabela[temp]){
 			if(tabela[temp1+1]==tabela[temp+1]){
@@ -14,8 +14,8 @@ int badania(int a, int temp, char tabela[]){
 	return wynik;
 }
 main(){
-	char tabela[1000],porownywarka[1000],znak,znak1;
-	int a=0,b,c,d,temp,temp1,temp2;
+	char tabela[1000],porownywarka[1000][1000],znak,znak1;
+	int a=0,b,c,d,temp,temp1,temp2,juz_sprawdzane;
 //Podawanie tekstu
 	printf("Podaj tekst\n");
 	znak=getchar();
@@ -28,7 +28,17 @@ main(){
 	}
 //Koniec podawania tekstu
 //Sprawdzanie i badanie mozliwosci par liter
-	for(temp=0;temp<=a;temp++){
-		printf("%c%c - wystapilo %i razy\n",tabela[temp],tabela[temp+1],badania(a,temp,tabela));
+	for(temp=0;temp<a-1;temp++){
+		for(temp1=0;temp1<temp;temp1++){
+			if(porownywarka[temp1][0]==tabela[temp] && porownywarka[temp1][1]==tabela[temp+1]){
+				juz_sprawdzane=1;
+			}
+		}
+		if(juz_sprawdzane==0){
+			porownywarka[temp][0]=tabela[temp];
+			porownywarka[temp][1]=tabela[temp+1];
+			printf("%c%c - wystapilo %i razy\n",tabela[temp],tabela[temp+1],badania(a,temp,tabela));
+		}
+		juz_sprawdzane=0;
 	}
 }
