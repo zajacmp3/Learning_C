455c856ae1216ab0ff4fbcf15559aacb703d4136
Mateusz Zajac <deejay.zajac@gmail.com>
2012-02-08 13:07:46 +0100

    wczesniej napisany program

---
+++ lab9-1a.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+
+#define dol_x -0.9
+#define gora_x 8
+#define krok_x 0.1
+
+
+float funkcja(float x){
+      float wynik;
+      wynik=(x*x*x)/27 - 2*(x*x)/9 + x/3 + 2;
+      return wynik;
+};
+
+main () {
+  float x, dol, gora, krok;
+  printf("\nTABLICOWANIE FUNKCJI:\n");
+  printf("\n Argument |   Wartosc   ");
+  printf("\n----------+-------------");
+  for (x=dol_x; x<=gora_x; x=x+krok_x)
+    printf("\n %8.4f | %10.4f ", x, funkcja(x));
+  printf("\n\n");
+}
+
